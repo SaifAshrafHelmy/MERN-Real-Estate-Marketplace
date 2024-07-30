da3200c7f1ab44e96153b6c6d50fbda140379db8
saifashrafhelmy <saifashrafhelmy@yahoo.com>
2024-07-30 13:28:36 +0300

    Basic template

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Profile from './pages/Profile.jsx';
+import SignIn from './pages/SignIn.jsx';
+import SignUp from './pages/SignUp.jsx';
+
 const App = () => {
     return (
-        <>
-            <h2 className="text-3xl text-red-500">App</h2>
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/sign-in" element={<SignIn />}></Route>
+                <Route path="/sign-up" element={<SignUp />}></Route>
+                <Route path="/about" element={<About />}></Route>
+                <Route path="/profile" element={<Profile />}></Route>
+            </Routes>
+        </BrowserRouter>
     );
 };
 
