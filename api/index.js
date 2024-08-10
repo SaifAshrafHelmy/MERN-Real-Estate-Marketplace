e68a0c9989e1e06b80bd7d6d545a4597b2b6d9fb
saifashrafhelmy <saifashrafhelmy@yahoo.com>
2024-08-10 15:24:45 +0300

    Configured mongodb basic connection

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO_DB_URI)
+    .then(() => {
+        console.log('Connected to MongoDB successfully.');
+    })
+    .catch((err) => {
+        console.log('Failed to connect to MongoDB', err);
+    });
 
 const app = express();
 const PORT = 3000;
